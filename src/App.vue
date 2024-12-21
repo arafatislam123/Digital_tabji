646ce510b4b75c79a5db74c9419cc50e3035800e
arafatislam123 <arafatpstu7@gmail.com>
2024-12-21 22:33:54 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue' 
+const number = ref(100)
+const increment = () => {
+  number.value++
+}
+const decrement = () => {
+  number.value--
+}
+const reset = () => {
+  number.value = 100
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ number }}</h1>
+    <button @click="increment">Increment</button>
+  </div>
+  <div>
+    <button @click="decrement">Minus</button>
+    <button @click="reset">Reset</button>
+  </div>
+</template>
+
+<style scoped>
+</style>
