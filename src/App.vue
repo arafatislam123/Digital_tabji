8e7081bcd45a4031bc79c044f14192ad76d9af92
arafatislam123 <arafatpstu7@gmail.com>
2024-12-22 01:25:10 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <script setup>
-import { ref } from 'vue' 
-const number = ref(100)
-const increment = () => {
-  number.value++
-}
-const decrement = () => {
-  number.value--
-}
-const reset = () => {
-  number.value = 100
+import { ref } from 'vue';
+const products = ref([]);
+
+CallApi();
+async function CallApi() {
+  const response = await fetch('https://dummyjson.com/products');
+  const data = await response.json();
+  products.value = data.products; // Access the products array
+  console.log(data);
 }
 </script>
 
 <template>
-  <div>
-    <h1>{{ number }}</h1>
-    <button @click="increment">Increment</button>
-  </div>
-  <div>
-    <button @click="decrement">Minus</button>
-    <button @click="reset">Reset</button>
-  </div>
+  <table>
+    <thead>
+    <tr>
+      <th>Id</th>
+      <th>Name</th>
+      <th>Price</th>
+      <th>Category</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="product in products" :key="product.id">
+      <td>{{ product.id }}</td>
+      <td>{{ product.title }}</td>
+      <td>{{ product.price }}</td>
+      <td>{{ product.category }}</td>
+    </tr>
+    </tbody>
+  </table>
 </template>
 
 <style scoped>
